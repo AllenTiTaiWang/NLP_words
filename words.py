06064ff5b607fd8916332a65ef8a03867e83b285
Ti-Tai Wang <titai.wang@gmail.com>
2019-01-21 14:46:11 -0700

    finish code

--- words.py
+++ words.py
@@ -1,6 +1,10 @@
 from typing import List, Text, Tuple
 import numpy as np
-
+from collections import Counter
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+import heapq
+from operator import itemgetter
 
 def most_common(word_pos_path: Text,
                 word_regex=".*",
@@ -30,6 +34,24 @@ def most_common(word_pos_path: Text,
     words, part-of-speech tags, or both.
     """
 
+    with open(word_pos_path) as f:
+        data = f.read()
+        line = data.split()
+
+        if pos_regex is not None and word_regex is None:
+            line = [item.rsplit('/', 1)[1] for item in line]
+            line = [item for item in line if re.match(pos_regex, item) is not  None]
+        
+        elif pos_regex is None and word_regex is not None:
+            line = [item.rsplit('/', 1)[0] for item in line]
+            line = [item for item in line if re.match(word_regex, item) is not  None]
+
+        else:
+            line = [item for item in line if re.match(word_regex, item.rsplit('/', 1)[0]) is not  None]
+            line = [item for item in line if re.match(pos_regex, item.rsplit('/', 1)[1]) is not  None]
+ 
+        common = Counter(line).most_common(n)
+        return common
 
 class WordVectors(object):
     def __init__(self, word_vectors_path: Text):
@@ -41,6 +63,14 @@ class WordVectors(object):
 
             the 0.063380 -0.146809 0.110004 -0.012050 -0.045637 -0.022240
         """
+      
+        self.dic = {}
+        with open(word_vectors_path) as f:
+            for line in f:
+                word = line.split(' ', 1)[0]
+                vec = line.split(' ', 1)[1].strip()
+                vec_a = np.fromstring(vec, sep = " ")
+                self.dic[word] = vec_a
 
     def average_vector(self, words: List[Text]) -> np.ndarray:
         """Calculates the element-wise average of the vectors for the given
@@ -53,6 +83,10 @@ class WordVectors(object):
         :return: The element-wise average of the word vectors.
         """
 
+        words_v = [self.dic[item] for item in words]
+        vec = np.average(words_v, axis = 0)
+        return vec
+
     def most_similar(self, word: Text, n=10) -> List[Tuple[Text, int]]:
         """Finds the most similar words to a query word. Similarity is measured
         by cosine similarity (https://en.wikipedia.org/wiki/Cosine_similarity)
@@ -62,3 +96,12 @@ class WordVectors(object):
         :param n: The number of most similar words to return.
         :return: The n most similar words to the query word.
         """
+        #heapq.nlargest
+        #scikit-learn
+        word_vec = self.dic[word]
+        cs_list = []
+        for key, value in self.dic.items():
+            cs = cosine_similarity(word_vec.reshape(1, -1), value.reshape(1, -1))
+            cs_list.append((key, float(cs)))
+        result = heapq.nlargest(n+1, cs_list, key = itemgetter(1))[1:]
+        return result
